868f223f6ea14afcba594b304919f2feef7eca0a
omani <3346207+omani@users.noreply.github.com>
2023-08-13 14:02:35 +0300

    fixes #1

--- utils.go
+++ utils.go
@@ -48,11 +48,6 @@ var _ age.Identity = &PasswordEncryptScryptIdentity{}
 
 // Unwrap unwraps given password and delegates it to NewScriptIdentity()
 func (i *PasswordEncryptScryptIdentity) Unwrap(stanzas []*age.Stanza) (fileKey []byte, err error) {
-	for _, s := range stanzas {
-		if s.Type == "scrypt" && len(stanzas) != 1 {
-			return nil, errors.New("an scrypt recipient must be the only one")
-		}
-	}
 	if len(stanzas) != 1 || stanzas[0].Type != "scrypt" {
 		return nil, age.ErrIncorrectIdentity
 	}
@@ -84,22 +79,6 @@ func encrypt(recipients []age.Recipient, in io.Reader, out io.Writer) error {
 	return nil
 }
 
-//	func encryptAccount(account *nkn.Account, password []byte) ([]byte, error) {
-//		r, err := age.NewScryptRecipient(string(password))
-//		if err != nil {
-//			return nil, err
-//		}
-//
-//		in := new(bytes.Buffer)
-//		err = json.NewEncoder(in).Encode(account)
-//		if err != nil {
-//			return nil, err
-//		}
-//		out := &bytes.Buffer{}
-//		encrypt([]age.Recipient{r}, in, out)
-//
-//		return out.Bytes(), nil
-//	}
 func decrypt(identities []age.Identity, in io.Reader, out io.Writer) error {
 	rr := bufio.NewReader(in)
 	if intro, _ := rr.Peek(len(crlfMangledIntro)); string(intro) == crlfMangledIntro ||
@@ -174,29 +153,6 @@ func decryptAccountByIdentityFile(walletfile *Wallet, identity string) (*nkn.Acc
 
 	return nkn.NewAccount(seed)
 }
-
-// func decryptAccountByPassword(walletfile *Wallet, password []byte) (*nkn.Account, error) {
-// 	identities := []age.Identity{
-// 		&PasswordEncryptScryptIdentity{string(password)},
-// 	}
-//
-// 	in := bytes.NewBuffer([]byte(walletfile.Armor))
-// 	out := &bytes.Buffer{}
-//
-// 	err := decrypt(identities, armor.NewReader(in), out)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-//
-// 	account := &vault.Account{}
-// 	err = json.NewDecoder(out).Decode(account)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	seed := crypto.GetSeedFromPrivateKey(account.PrivateKey)
-//
-// 	return nkn.NewAccount(seed)
-// }
 
 func passphrasePromptForDecryption() (string, error) {
 	pass, err := password.GetPassword("Enter passphrase")
